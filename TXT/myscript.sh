239d61dc9bf994b9a8cdc707478ded3fb38e609a
mhanif-82 <54966161+mhanif-82@users.noreply.github.com>
2020-10-12 07:20:00 +0700

    Update myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-echo -n "Enter directory name ->"
-read newdir
-cmd="mkdir $newdir"
-eval $cmd
+
+FILES="mylog.txt mypubkey.txt myrank.txt myscript.sh"
+SHA="SHA256SUM"
+
+rm -f $SHA $SHA.asc
+
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
+
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
+
+echo "gpg -o $SHA.asc -a -sb $SHA"
+gpg -o $SHA.asc -a -sb $SHA
+
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
+
+git add .
+git commit -m"OS202 mhanif-82"
+git push origin master
+
+exit 0
