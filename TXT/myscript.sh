d7a204ffac5e255e067cd9e8087e21792a86427a
mhanif-82 <54966161+mhanif-82@users.noreply.github.com>
2020-10-19 09:52:19 +0700

    Update myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -3,6 +3,7 @@
 FILES="my*.txt my*.sh"
 SHA="SHA256SUM"
 
+echo "rm -f $SHA $SHA.asc"
 rm -f $SHA $SHA.asc
 
 echo "sha256sum $FILES > $SHA"
@@ -22,3 +23,8 @@ git commit -m"OS202 mhanif-82"
 git push origin master
 
 exit 0
+
+# Mon Sep 28 21:05:04 WIB 2020
+# Tue 29 Sep 2020 11:02:39 AM WIB
+
+
